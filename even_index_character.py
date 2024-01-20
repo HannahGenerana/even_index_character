55b4ced27a7db33c3d8770c024844f83eb6de31a
HannahGenerana <hannahgenerana@gmail.com>
2024-01-20 23:10:19 +0800

    pseudocode

---
+++ even_index_character.py
@@ -0,0 +1,14 @@
+# Exercise 3: Print characters from a string that are present at an even index number
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+# pseudocode
+
+# ask user to input a word
+
+# print the word 
+
+# determine the length of the word
+
+# only present even index number
+
+# print all even index character
